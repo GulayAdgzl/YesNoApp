93fb56517841ce4e1ddd7960b6f15cda4acb0e28
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-03-13 00:02:52 +0300

    Add Dagger Hilt module for providing Retrofit and OkHttpClient dependencies

--- app/src/main/java/com/glyadgzl/random/di/RandomYesNoModel.kt
+++ app/src/main/java/com/glyadgzl/random/di/RandomYesNoModel.kt
@@ -1,4 +1,47 @@
 package com.glyadgzl.random.di
-
+import com.glyadgzl.random.api.YesNoAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+@Module
+@InstallIn(SingletonComponent::class)
 class RandomYesNoModel {
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return interceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://yesno.wtf/") // BASEURL yerine doğrudan kullanılacak URL yazıldı
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideYesNoAPI(retrofit: Retrofit): YesNoAPI {
+        return retrofit.create(YesNoAPI::class.java)
+    }
 }
