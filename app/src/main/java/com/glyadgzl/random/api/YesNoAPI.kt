f417d573ed094abe86cad593ebd92754eae26e10
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-03-12 23:28:18 +0300

    Convert YesNoAPI class to interface and add getRandomYesNo API call

--- app/src/main/java/com/glyadgzl/random/api/YesNoAPI.kt
+++ app/src/main/java/com/glyadgzl/random/api/YesNoAPI.kt
@@ -1,4 +1,8 @@
 package com.glyadgzl.random.api
-
-class YesNoAPI {
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+interface YesNoAPI {
+    @GET("/api")
+    suspend fun getRandomYesNo(): Response<YesNoAPI>
 }
