f33d7a0bd5ad3ac6dd764e7d06e253015d4ac648
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-03-12 23:40:50 +0300

    Enhance YesNoRepository with LiveData and StateFlow for response handling

--- app/src/main/java/com/glyadgzl/random/repository/YesNoRepository.kt
+++ app/src/main/java/com/glyadgzl/random/repository/YesNoRepository.kt
@@ -1,4 +1,45 @@
 package com.glyadgzl.random.repository
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.glyadgzl.random.api.YesNoAPI
+import com.glyadgzl.random.model.YesNoResponse
 
-class YesNoRepository {
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+class YesNoRepository @Inject constructor(private val randomYesNoAPI:YesNoAPI){
+
+    private val _getYesNoResponse=MutableStateFlow<YesNoResponse?>(null)
+
+    val getYesNoResponse:StateFlow<YesNoResponse?> get()=_getYesNoResponse
+
+
+    suspend fun getYesNoResponse(){
+        val data=randomYesNoAPI.getRandomYesNo()
+        if(data.isSuccessful&&data.body()!=null){
+            _getYesNoResponse.emit(data.body())
+        }
+    }
+
+    private val _getRandomLiveData=MutableLiveData<YesNoResponse?>(null)
+    val getRandomLiveData: LiveData<YesNoResponse?> get()=_getRandomLiveData
+
+    suspend fun getRandomUserLiveData(){
+        val data=randomYesNoAPI.getRandomYesNo()
+        if(data.isSuccessful&&data.body()!=null){
+            _getRandomLiveData.postValue(data.body())
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
