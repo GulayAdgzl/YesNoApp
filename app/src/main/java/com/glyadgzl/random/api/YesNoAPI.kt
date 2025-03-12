ade0743edce04c8f3e7c0d00aa56d922879a7bd6
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-03-12 23:43:42 +0300

    Fix return type in YesNoAPI and refactor YesNoRepository for improved response handling

--- app/src/main/java/com/glyadgzl/random/api/YesNoAPI.kt
+++ app/src/main/java/com/glyadgzl/random/api/YesNoAPI.kt
@@ -1,8 +1,9 @@
 package com.glyadgzl.random.api
+import com.glyadgzl.random.model.YesNoResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 interface YesNoAPI {
     @GET("/api")
-    suspend fun getRandomYesNo(): Response<YesNoAPI>
+    suspend fun getRandomYesNo(): Response<YesNoResponse> // Burada dönüş tipi düzeltildi
 }
--- app/src/main/java/com/glyadgzl/random/repository/YesNoRepository.kt
+++ app/src/main/java/com/glyadgzl/random/repository/YesNoRepository.kt
@@ -8,38 +8,25 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
-class YesNoRepository @Inject constructor(private val randomYesNoAPI:YesNoAPI){
+class YesNoRepository @Inject constructor(private val randomYesNoAPI: YesNoAPI) {
 
-    private val _getYesNoResponse=MutableStateFlow<YesNoResponse?>(null)
+    private val _getYesNoResponse = MutableStateFlow<YesNoResponse?>(null)
+    val getYesNoResponse: StateFlow<YesNoResponse?> get() = _getYesNoResponse
 
-    val getYesNoResponse:StateFlow<YesNoResponse?> get()=_getYesNoResponse
-
-
-    suspend fun getYesNoResponse(){
-        val data=randomYesNoAPI.getRandomYesNo()
-        if(data.isSuccessful&&data.body()!=null){
+    suspend fun getYesNoResponse() {
+        val data = randomYesNoAPI.getRandomYesNo()
+        if (data.isSuccessful && data.body() != null) {
             _getYesNoResponse.emit(data.body())
         }
     }
 
-    private val _getRandomLiveData=MutableLiveData<YesNoResponse?>(null)
-    val getRandomLiveData: LiveData<YesNoResponse?> get()=_getRandomLiveData
+    private val _getRandomLiveData = MutableLiveData<YesNoResponse?>()
+    val getRandomLiveData: LiveData<YesNoResponse?> get() = _getRandomLiveData
 
-    suspend fun getRandomUserLiveData(){
-        val data=randomYesNoAPI.getRandomYesNo()
-        if(data.isSuccessful&&data.body()!=null){
+    suspend fun getRandomUserLiveData() {
+        val data = randomYesNoAPI.getRandomYesNo()
+        if (data.isSuccessful && data.body() != null) {
             _getRandomLiveData.postValue(data.body())
         }
     }
-
-
-
-
-
-
-
-
-
-
-
 }
