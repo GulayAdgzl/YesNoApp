82803a2e3b6e35f74a8f8fe7dba6ba74ccac8fd6
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-03-13 00:26:01 +0300

    Refactor MainActivity to integrate YesNoScreen and ViewModel, removing Greeting function

--- app/src/main/java/com/glyadgzl/random/MainActivity.kt
+++ app/src/main/java/com/glyadgzl/random/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.glyadgzl.random.di.RandomYesNoModel
+import com.glyadgzl.random.model.YesNoResponse
 import com.glyadgzl.random.ui.theme.RandomTheme
+import com.glyadgzl.random.viewmodels.YesNoViewModel
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.glyadgzl.random.screens.YesNoScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +30,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RandomTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold() {
+                    Box(modifier = Modifier.padding(it).background(Color.White)){
+                        // Provide ViewModel
+                        val viewModel: YesNoViewModel = hiltViewModel()
+                        callAPI(viewModel)
+                        val randomData: State<YesNoResponse?> = viewModel.randomData.collectAsState()
+
+
+
+                        YesNoScreen(randomData)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RandomTheme {
-        Greeting("Android")
     }
 }
