d9abf97719824c031c8dd2b0dd54c93296818b8e
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-03-16 13:52:42 +0300

    Refactor MainActivity to simplify UI setup and remove unused API call logic

--- app/src/main/java/com/glyadgzl/random/MainActivity.kt
+++ app/src/main/java/com/glyadgzl/random/MainActivity.kt
@@ -34,62 +34,21 @@ import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.StyledPlayerView
 import kotlinx.coroutines.launch
-/*
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            RandomTheme {
-              Surface(
-                  modifier=Modifier.fillMaxSize(),
-                  color= MaterialTheme.colorScheme.background
-              ) {
-                  Box(modifier = Modifier.padding(it).background(Color.White)){
-                      // Provide ViewModel
-                      val viewModel: YesNoViewModel = hiltViewModel()
-                      callAPI(viewModel)
-                      val randomData: State<YesNoResponse?> = viewModel.randomData.collectAsState()
-
-                      YesNoScreen(getVideoUri(),randomData)
-
-                  }
-
-              }
-            }
-        }
-    }
-
-    private fun getVideoUri(): Uri {
-        val rawId = resources.getIdentifier("clouds", "raw", packageName)
-        val videoUri = "android.resource://$packageName/$rawId"
-        return Uri.parse(videoUri)
-    }
-
-
-    private fun callAPI(viewModel: YesNoViewModel) {
-        viewModel.viewModelScope.launch {
-            viewModel.getRandomData()
-        }
-    }
-}
-*/
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge() // Tam ekran görünüm
         setContent {
-            RandomTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    YesNoScreen(getVideoUri()) // Ana ekran çağrılıyor
-                }
+          YesnoTheme   {
+              Surface(
+                  modifier = Modifier.fillMaxSize(),
+                  color = MaterialTheme.colorScheme.background
+
+              ) {
+                  YesNoScreen(getVideoUri())
+
+              }
             }
         }
     }
